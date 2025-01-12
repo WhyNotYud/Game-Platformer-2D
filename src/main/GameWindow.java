0fa2d37810e5feeaa79c018363091b74a54d8640
WhyNotYud <mtd.contactwithme@gmail.com>
2025-01-12 15:40:28 +0700

    feat: ✨️add GameWindow class to create main game window

---
+++ src/main/GameWindow.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.*;
+
+// Cửa sổ game
+public class GameWindow extends JFrame {
+    private JFrame jFrame;
+
+    public GameWindow(GamePanel gamePanel) {
+        jFrame = new JFrame();
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.add(gamePanel);
+        jFrame.setVisible(true);
+    }
+}
