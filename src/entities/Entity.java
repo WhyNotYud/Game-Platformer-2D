e2b3a2d38e6fa7de5e591270599f745263fc2ab7
WhyNotYud <mtd.contactwithme@gmail.com>
2025-01-13 11:42:43 +0700

    feat: ✨ add base Entity class for game objects

---
+++ src/entities/Entity.java
@@ -0,0 +1,13 @@
+package entities;
+
+public abstract class Entity {
+    // Lưu tọa độ của đối tượng
+    protected float x, y;
+
+    public Entity(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
