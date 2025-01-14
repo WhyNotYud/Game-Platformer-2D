8d3e157b65a89f469b4fc64b17f6a60c46c6b6f4
WhyNotYud <mtd.contactwithme@gmail.com>
2025-01-14 18:07:52 +0700

    feat: 🎉 add jump functionality with space key

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -13,40 +13,48 @@ public class KeyboardInputs implements KeyListener {
         this.gamePanel = gamePanel;
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_A:
-                gamePanel.getGame().getPlayer().setLeft(false);
-                break;
-            case KeyEvent.VK_S:
-                gamePanel.getGame().getPlayer().setDown(false);
-                break;
-            case KeyEvent.VK_D:
-                gamePanel.getGame().getPlayer().setRight(false);
-                break;
-            case KeyEvent.VK_W:
-                gamePanel.getGame().getPlayer().setUp(false);
-                break;
-        }
-    }
-
     /// Thay đổi vị trí của nhân vật khi bấm các nút W, A, S, D
     /// Các phím W, A, S, D hoặc các phím mũi tên được ánh xạ để di chuyển lên, xuống, trái, phải.
     @Override
     public void keyPressed(KeyEvent e) {
+        System.out.println("Key: " + e.getKeyChar());
         switch (e.getKeyCode()) {
             case KeyEvent.VK_A:
                 gamePanel.getGame().getPlayer().setLeft(true);
                 break;
-            case KeyEvent.VK_S:
-                gamePanel.getGame().getPlayer().setDown(true);
-                break;
+//            case KeyEvent.VK_S:
+//                gamePanel.getGame().getPlayer().setDown(true);
+//                break;
             case KeyEvent.VK_D:
                 gamePanel.getGame().getPlayer().setRight(true);
                 break;
-            case KeyEvent.VK_W:
-                gamePanel.getGame().getPlayer().setUp(true);
+//            case KeyEvent.VK_W:
+//                gamePanel.getGame().getPlayer().setUp(true);
+//                break;
+            case KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(true);
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("Key: " + e.getKeyChar());
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_A:
+                gamePanel.getGame().getPlayer().setLeft(false);
+                break;
+//            case KeyEvent.VK_S:
+//                gamePanel.getGame().getPlayer().setDown(false);
+//                break;
+            case KeyEvent.VK_D:
+                gamePanel.getGame().getPlayer().setRight(false);
+                break;
+//            case KeyEvent.VK_W:
+//                gamePanel.getGame().getPlayer().setUp(false);
+//                break;
+            case KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(false);
                 break;
         }
     }
