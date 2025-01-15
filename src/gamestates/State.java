bd59d9de68dd8062e79af08d679610e2fa78ded2
WhyNotYud <mtd.contactwithme@gmail.com>
2025-01-15 22:51:58 +0700

    feat: 🛠️ implement basic State class structure

---
+++ src/gamestates/State.java
@@ -0,0 +1,15 @@
+package gamestates;
+
+import main.Game;
+
+public class State {
+    protected Game game;
+
+    public State(Game game) {
+
+    }
+
+    public Game getGame() {
+        return game;
+    }
+}
